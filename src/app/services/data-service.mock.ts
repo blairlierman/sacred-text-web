5f4da756de3c8f6b1f8aa84566046df64d6e4307
Blair Lierman <blairlierman@gmail.com>
2020-10-13 21:37:45 -0700

    Add initial data for data service

---
+++ src/app/services/data-service.mock.ts
@@ -0,0 +1,22 @@
+export class DataServiceMock {
+    category = [{
+        id: 0,
+        name: "Prologue",
+        description: "The introduction"
+    }];
+
+    category_subcategory = [
+        {
+            categoryId: 0,
+            subCategoryId: 0
+        }
+    ];
+
+    subCategory = [
+        {
+            id: 0,
+            name: "General Beliefs",
+            description: "General Beliefs common to all Religions"
+        }   
+    ]
+}
