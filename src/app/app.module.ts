3e10959daf7c7aef02b21b2aa0df6da7d98faa01
Blair Lierman <blairlierman@gmail.com>
2020-11-02 19:30:57 -0800

    Add a new SubCategories Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMainComponent } from './components/app-main/app-main.component';
+import { AppSubcategoriesComponent } from './components/app-subcategories/app-subcategories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppMainComponent
+    AppMainComponent,
+    AppSubcategoriesComponent
   ],
   imports: [
     BrowserModule,
