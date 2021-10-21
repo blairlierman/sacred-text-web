c7a00b8adcc0a50e40357da1c408ceb3492650c4
Blair Lierman <blairlierman@gmail.com>
2021-10-20 20:34:17 -0700

    Use Angular Router for Back Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavigationService } from './services/navigation/navigation.service';
 
 @Component({
@@ -9,5 +10,10 @@ import { NavigationService } from './services/navigation/navigation.service';
 export class AppComponent {
   title = 'sacred-text-web';
 
-  constructor(public navigation: NavigationService) {}
+  constructor(public navigation: NavigationService, public router: Router) {}
+
+  goBack() {
+    if(this.router.url.match(/categories/)) { this.router.navigate(['/']);}
+    else { this.router.navigate(['/']);}
+  }
 }
