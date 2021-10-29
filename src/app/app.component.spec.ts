33d9912911d0411aba9d246e84df06262e7b8b02
Blair Lierman <blairlierman@gmail.com>
2021-10-28 20:21:33 -0700

    Have Title update based on selected Category. Set initial and home Title to 'Sacred Text Lite'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sacred-text-web'`, () => {
+  it(`should have as title 'Sacred Text Lite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sacred-text-web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sacred-text-web app is running!');
+    expect(app.appTitle).toEqual('Sacred Text Lite');
   });
 });
