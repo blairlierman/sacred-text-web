8a90d29e29b7d1acacd7e83e50af8c2abd2a0c9a
Blair Lierman <blairlierman@gmail.com>
2021-10-31 12:55:14 -0700

    Load Passages based on SubcategoryID in URL

--- src/app/components/passages/passages.component.ts
+++ src/app/components/passages/passages.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Passage } from 'src/app/data/models/passage';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { NavigationService } from 'src/app/services/navigation/navigation.service';
 import { PassageService } from 'src/app/services/passage/passage.service';
@@ -10,15 +12,19 @@ import { PassageService } from 'src/app/services/passage/passage.service';
   styleUrls: ['./passages.component.scss']
 })
 export class PassagesComponent implements OnInit {
-
+  passages$: Observable<Passage[]> | undefined;
+  
   constructor(private passageService: PassageService, private categoryService: CategoryService, private navigation: NavigationService,
     private route: ActivatedRoute) { 
     this.navigation.backButtonVisibility(true);
     const subcategoryId = this.route.snapshot.paramMap.get('id');
-    if(subcategoryId != null) { this.navigation.setSubcategory(parseInt(subcategoryId)); }
+    if(subcategoryId != null) { 
+      const subcategoryIdNumber = parseInt(subcategoryId);
+      this.navigation.setSubcategory(subcategoryIdNumber); 
+      this.passages$ = this.passageService.getPassages(subcategoryIdNumber);
+    }
   }
 
-  passages$ = this.passageService.getPassages(1);
 
   ngOnInit(): void {
   }
